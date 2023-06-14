d20105bd69ffb29ec889dc4901f7bb0df0d3112f
Tyomichh <workemailaertemlozenko159@gmail.com>
2023-06-14 11:23:46 +0300

    Redesigned the burger menu, made some changes

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,8 @@
 const menuBTN = document.querySelector('.menu-burger');
 const menU = document.querySelector('.menu-list');
-const arroW = document.querySelector('.arrow');
 
 menuBTN.addEventListener('click', (e) => {
     menU.classList.toggle('active');
-    arroW.classList.toggle('active');
 })
 
 document.addEventListener('click', (e) => {
@@ -12,6 +10,5 @@ document.addEventListener('click', (e) => {
 
     if (!click) {
         menU.classList.remove('active');
-        arroW.classList.remove('active'); 
     }
 })
