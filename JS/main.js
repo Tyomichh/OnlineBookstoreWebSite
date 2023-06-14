eab3bec5ee15f6ca4550d06df478d11497cdb189
Tyomichh <workemailaertemlozenko159@gmail.com>
2023-06-14 16:52:54 +0300

    Add smooth scroll and section 3

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,50 @@
-const menuBTN = document.querySelector('.menu-burger');
-const menU = document.querySelector('.menu-list');
+const menuBTN = $('.menu-burger'),
+      menWrp = $('.menu-wrapper'),
+      menLst = $('.menu-list'),
+      menLnk = $('.menu-list-link'),
+      menInp = $('.menu-input');
+// let elements = $('[id]');
+// const idArray = [];
 
-menuBTN.addEventListener('click', (e) => {
-    menU.classList.toggle('active');
-})
+// /*Adding id tags for navigatiton to the arr*/ 
+// elements.each(function() {
+//     if (this.id.endsWith("_tag")) {
+//       idArray.push(this.id);
+//     }
+//   });
+  
+// console.log(idArray);
 
-document.addEventListener('click', (e) => {
-    const click = e.composedPath().includes(menuBTN);
+/*Navigation burger*/ 
+menuBTN.on('click', (e) => {
+  menWrp.toggleClass('active-wrap');
+});
 
-    if (!click) {
-        menU.classList.remove('active');
-    }
-})
+$(document).on('click', (e) => {
+  const click = e.originalEvent.composedPath().includes(menuBTN.get(0));
+  const clickedEl = e.target;
+  const isMenLnk = Array.from(menLnk).some(link => link.contains(clickedEl));
+
+  if (clickedEl === menLst.get(0) || clickedEl === menInp.get(0) || isMenLnk) {
+    return;
+  } else if (!click) {
+    menWrp.removeClass('active-wrap');
+  }
+});
+
+ /*Smooth scroll*/
+ $('[data-scroll]').on('click', function (event) {
+     event.preventDefault();
+
+     var blockID = $(this).data('scroll'),
+         blockOffset = $(blockID).offset().top;
+
+         $('.menu-list-link').removeClass('active-list');
+         $(this).addClass('active-list');
+
+     $('html, body').animate({
+         scrollTop: blockOffset
+     }, 500);
+     
+ });
+ 
