29851537679f35d208445e7358f763267edadfdc
Tyomichh <workemailaertemlozenko159@gmail.com>
2023-06-16 00:26:55 +0300

    Add JS show more - show less btn

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,21 @@
 const menuBTN = $('.menu-burger'),
-      menWrp = $('.menu-wrapper'),
-      menLst = $('.menu-list'),
-      menLnk = $('.menu-list-link'),
-      menInp = $('.menu-input');
-// let elements = $('[id]');
-// const idArray = [];
+  menWrp = $('.menu-wrapper'),
+  menLst = $('.menu-list'),
+  menLnk = $('.menu-list-link'),
+  menInp = $('.menu-input');
+let elements = $('[id]');
+const idArray = [];
 
-// /*Adding id tags for navigatiton to the arr*/ 
-// elements.each(function() {
-//     if (this.id.endsWith("_tag")) {
-//       idArray.push(this.id);
-//     }
-//   });
-  
-// console.log(idArray);
+/*Adding id tags for navigatiton to the arr*/ 
+elements.each(function() {
+    if (this.id.endsWith("_tag")) {
+      idArray.push(this.id);
+    }
+  });
 
-/*Navigation burger*/ 
+console.log(idArray);
+
+/*Navigation burger*/
 menuBTN.on('click', (e) => {
   menWrp.toggleClass('active-wrap');
 });
@@ -32,19 +32,57 @@ $(document).on('click', (e) => {
   }
 });
 
- /*Smooth scroll*/
- $('[data-scroll]').on('click', function (event) {
-     event.preventDefault();
+/*Smooth scroll*/
+$('[data-scroll]').on('click', function (event) {
+  event.preventDefault();
 
-     var blockID = $(this).data('scroll'),
-         blockOffset = $(blockID).offset().top;
+  var blockID = $(this).data('scroll'),
+    blockOffset = $(blockID).offset().top;
 
-         $('.menu-list-link').removeClass('active-list');
-         $(this).addClass('active-list');
+  $('.menu-list-link').removeClass('active-list');
+  $(this).addClass('active-list');
 
-     $('html, body').animate({
-         scrollTop: blockOffset
-     }, 500);
-     
- });
- 
+  $('html, body').animate({
+    scrollTop: blockOffset
+  }, 500);
+
+});
+
+
+let contProductCntnrs = $('.product-container').length,
+  start = 9,
+  show = 3,
+  fullend = $('.product-container').length;
+
+$('.product-container').addClass('d_none');
+$('.product-container:lt(' + start + ')').removeClass('d_none');
+
+$(document).on('click', '.btn-show_more', function () {
+
+  start = (start + show <= contProductCntnrs) ? start + show : contProductCntnrs;
+
+  $('.product-container:lt(' + start + ')').removeClass('d_none');
+
+  if ($('.product-container:not(.d_none)').length === contProductCntnrs) {
+
+    $('#showFirst').addClass('btn-show_less');
+    $('#showFirst').removeClass('btn-show_more');
+
+  }
+
+});
+
+$(document).on('click', '.btn-show_less', function () {
+
+  start = 9;
+
+  fullend = (fullend <= 9) ? $('.product-container').length : fullend;
+  fullend = (fullend >= start) ? fullend - show : start;
+  $('.product-container:gt(' + (fullend - 1) + ')').addClass('d_none');
+
+
+  if ($('.d_none').length === (start - 3)) {
+    $('#showFirst').addClass('btn-show_more');
+    $('#showFirst').removeClass('btn-show_less');
+  }
+});
